31aeff6fec61c0cc0e430d576dceb7be2bdf4578
MARK\pirat <marktbss@gmail.com>
2024-03-27 06:45:03 +0700

    [ADD] Config Package

--- databases/database.go
+++ databases/database.go
@@ -1,12 +1,30 @@
 package databases
 
-import "os"
+import (
+	"database/sql"
+	"log"
 
-func DatabaseURL() string {
-	return "host=" + os.Getenv("DATABASE_HOST") +
-		" port=" + os.Getenv("DATABASE_PORT") +
-		" user=" + os.Getenv("DATABASE_USERNAME") +
-		" password=" + os.Getenv("DATABASE_PASSWORD") +
-		" dbname=" + os.Getenv("DATABASE_DATABASE") +
-		" sslmode=" + os.Getenv("DATABASE_SSL_MODE")
+	"github.com/MarkTBSS/assessment/configs"
+	_ "github.com/lib/pq"
+)
+
+func ConnectDatabase() error {
+	database, err := sql.Open("postgres", configs.DatabaseURL())
+	if err != nil {
+		log.Fatal("Connect to database error", err)
+	}
+	defer database.Close()
+	createTable := `CREATE TABLE IF NOT EXISTS expenses (
+		id SERIAL PRIMARY KEY,
+		title TEXT,
+		amount INT,
+		note TEXT,
+		tags TEXT[]
+	);`
+	_, err = database.Exec(createTable)
+
+	if err != nil {
+		log.Fatal("can't create table", err)
+	}
+	return err
 }
--- main.go
+++ main.go
@@ -1,35 +1,11 @@
 package main
 
 import (
-	"database/sql"
-	"log"
-
+	"github.com/MarkTBSS/assessment/configs"
 	"github.com/MarkTBSS/assessment/databases"
-	"github.com/joho/godotenv"
-	_ "github.com/lib/pq"
 )
 
 func main() {
-	//log.Fatal("Error loading .env file: ", godotenv.Load("development.env"))
-	err := godotenv.Load("development.env")
-	if err != nil {
-		log.Fatal("Error loading .env file")
-	}
-	database, err := sql.Open("postgres", databases.DatabaseURL())
-	if err != nil {
-		log.Fatal("Connect to database error", err)
-	}
-	defer database.Close()
-	createTable := `CREATE TABLE IF NOT EXISTS expenses (
-		id SERIAL PRIMARY KEY,
-		title TEXT,
-		amount INT,
-		note TEXT,
-		tags TEXT[]
-	);`
-	_, err = database.Exec(createTable)
-
-	if err != nil {
-		log.Fatal("can't create table", err)
-	}
+	configs.LoadConfig()
+	databases.ConnectDatabase()
 }
